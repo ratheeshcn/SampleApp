48b84743e3bb11b0996ed60b7324232c6ef77cee
Retheesh CN <androidrcn@gmail.com>
2017-10-07 21:08:20 +0400

    update `bindingAdapters`

--- app/src/main/java/com/intigral/ui/util/binding/BindingAdapters.java
+++ app/src/main/java/com/intigral/ui/util/binding/BindingAdapters.java
@@ -2,20 +2,10 @@ package com.intigral.ui.util.binding;
 
 import android.databinding.BindingAdapter;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.ShapeDrawable;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.DividerItemDecoration;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -74,32 +64,6 @@ public final class BindingAdapters {
         Glide.with(imageView.getContext()).load(filepath).into(imageView);
     }
 
-    /**
-     * Scroll to.
-     *
-     * @param scrollView the scroll view
-     * @param isEnable   the is enable
-     */
-    @BindingAdapter("scrollTo")
-    public static void scrollTo(@NonNull NestedScrollView scrollView, @Nullable boolean isEnable) {
-        if (isEnable) {
-
-            scrollView.postDelayed(new Runnable() {
-                public void run() {
-                    scrollView.smoothScrollTo(0, scrollView.getBottom());
-
-                }
-            }, 500L);
-
-        }else{
-            scrollView.postDelayed(new Runnable() {
-                public void run() {
-                    scrollView.smoothScrollTo(0, scrollView.getTop());
-
-                }
-            }, 500L);
-        }
-    }
 
     /**
      * Scroll to.
@@ -145,49 +109,9 @@ public final class BindingAdapters {
 
 
 
-    /**
-     * Sets background color.
-     *
-     * @param view  the view
-     * @param color the color
-     */
-    @BindingAdapter("bg_color")
-    public static void setBackgroundColor(@NonNull View view, int color) {
-        Drawable background = view.getBackground();
-        if (background instanceof ShapeDrawable) {
-            ((ShapeDrawable) background).getPaint().setColor(color);
-        } else if (background instanceof GradientDrawable) {
-            ((GradientDrawable) background).setColor(color);
-        } else if (background instanceof ColorDrawable) {
-            ((ColorDrawable) background).setColor(color);
-        }
-    }
 
 
 
-    /**
-     * Sets src color.
-     *
-     * @param view  the view
-     * @param color the color
-     */
-    @BindingAdapter("src_color")
-    public static void setSrcColor(@NonNull ImageView view, int color) {
-        Drawable drawable = view.getDrawable();
-        DrawableCompat.setTint(drawable, color);
-        view.setImageDrawable(drawable);
-    }
-
-    /**
-     * Sets text color.
-     *
-     * @param view  the view
-     * @param color the color
-     */
-    @BindingAdapter("text_color")
-    public static void setTextColor(@NonNull TextView view, int color) {
-        view.setTextColor(color);
-    }
 
 
 
@@ -214,21 +138,6 @@ public final class BindingAdapters {
         textView.setTypeface(checkNotNull(typeface));
     }
 
-    /**
-     * Sets view pager adapter.
-     *
-     * @param pager   the pager
-     * @param adapter the adapter
-     */
-    @BindingAdapter("viewpager_adapter")
-    public static void setViewPagerAdapter(@NonNull ClickableViewPager pager,
-                                           @NonNull PagerAdapter adapter) {
-        if(adapter !=null) {
-            pager.setAdapter(checkNotNull(adapter));
-            adapter.notifyDataSetChanged();
-        }
-    }
-
 
 
     /**
@@ -243,30 +152,7 @@ public final class BindingAdapters {
     }
 
 
-    /**
-     * Sets layout background color.
-     *
-     * @param view  the view
-     * @param color the color
-     */
-    @BindingAdapter("layout_bg_color")
-    public static void setLayoutBackgroundColor(@NonNull View view, int color) {
-       view.setBackgroundColor(color);
-    }
 
-    /**
-     * Sets list divider.
-     *
-     * @param view        the view
-     * @param orientation the orientation
-     */
-    @BindingAdapter("list_divider")
-    public static void setListDivider(@NonNull RecyclerView view, int orientation) {
-        if(orientation == 1){
-            view.addItemDecoration(new DividerItemDecoration(view.getContext(),
-                    LinearLayoutManager.HORIZONTAL));
-        }
-    }
 
 
 
